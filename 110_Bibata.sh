eccbfd40f8f01d2a0253f28d9d68e4a1824b8b91
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-05-22 10:24:00 +0100

    Update 110_Bibata.sh

--- 110_Bibata.sh
+++ 110_Bibata.sh
@@ -2,3 +2,9 @@
 
 cp -r Bibata-Modern-Ice ~/.icons
 # sudo cp -r Bibata-Modern-Ice /usr/share/icons
+
+echo "May need to change /home/phil/.config/gtk-3.0/settings.ini \n"
+echo " and /home/phil/.icons/default/index.theme"
+# May need to change /home/phil/.config/gtk-3.0/settings.ini
+
+# and /home/phil/.icons/default/index.theme
