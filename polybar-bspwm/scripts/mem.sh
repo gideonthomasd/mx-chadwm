bdec1dae2179e93a5ba93b5190e61bf8e20b6399
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-06-04 12:59:15 +0100

    Add files via upload

--- polybar-bspwm/scripts/mem.sh
+++ polybar-bspwm/scripts/mem.sh
@@ -3,6 +3,6 @@
 MEMUSED=$(free -h | awk '(NR==2) {print $3}')
 MEMTOT=$(free -h | awk '(NR==2) {print $2}')
 
-echo "%{T1} %{T-} %{T1} $MEMUSED %{T-}"
+echo "%{T1} $MEMUSED %{T-}"
 #$MEMTOT
 #"%{T3}%{T-} %{T2}$count%{T-}"
