08c78edf0cdbfbf2e7d8f5751b1add87b62e1045
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-08-12 21:53:58 +0100

    Update 100_Install_Chadwm.sh

--- 100_Install_Chadwm.sh
+++ 100_Install_Chadwm.sh
@@ -55,15 +55,16 @@ cp lxterminal.conf ~/.config/lxterminal
 
 cp log.sh ~/log.sh
 cp bashrc ~/.bashrc
+cp Xresources ~/.Xresources
 
 sudo apt install gdebi gdebi-core -y
 
 #sudo dpkg -i jgmenu_4.4.1-1_amd64.deb
-sudo gdebi jgmenu_4.4.1-1_amd64.deb
+#sudo gdebi jgmenu_4.4.1-1_amd64.deb
 
 ####Installation#########
 ###DEBIAN################
-sudo apt install i3lock-fancy lxpolkit rofi dmenu curl pcmanfm xfce4-terminal lxterminal volumeicon-alsa libx11-dev libxft-dev libimlib2 libimlib2-dev libxinerama-dev build-essential moc parcellite lolcat picom sxhkd lxappearance -y
+sudo apt install jgmenu i3lock-fancy lxpolkit rofi dmenu curl pcmanfm xfce4-terminal lxterminal volumeicon-alsa libx11-dev libxft-dev libimlib2 libimlib2-dev libxinerama-dev build-essential moc parcellite lolcat picom sxhkd lxappearance -y
 sudo apt install neofetch xdotool feh geany ffmpeg lxtask pavucontrol pulseaudio calcurse htop aptitude -y
 
 
