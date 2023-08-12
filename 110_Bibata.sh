6ae4ad92f2d414b2ed22ec120144991eb8712bfd
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-08-12 23:09:04 +0100

    Update 110_Bibata.sh

--- 110_Bibata.sh
+++ 110_Bibata.sh
@@ -15,11 +15,13 @@ echo " and /home/phil/.icons/default/index.theme"
 tar -xzvf candy-icons.tar.gz
 tar -xzvf Dracula.tar.gz
 tar -xzvf Sweet-Dark.tar.gz
+tar -xzvf kora.tar.gz
 
 
 cp -r candy-icons ~/.icons/candy-icons
 cp -r Dracula ~/.themes/Dracula
 cp -r Sweet-Dark ~/.themes/Sweet-Dark
+cp -r kora ~/.icons/kora
 
 mkdir -p ~/.local/share/rofi/themes
 cd rofithemes
