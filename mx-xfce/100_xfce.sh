42fd4959da78ad52ddaa1bbdff35c41f09036fd4
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-08-13 22:54:16 +0100

    Update 100_xfce.sh

--- mx-xfce/100_xfce.sh
+++ mx-xfce/100_xfce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install deadbeef plank pulseaudio volumeicon-alsa -y
+sudo apt install audacious plank pulseaudio volumeicon-alsa -y
 
 mkdir -p ~/.local/share/plank/themes
 
@@ -8,6 +8,7 @@ cd myplank
 cp -r * ~/.local/share/plank/themes
 cd ..
 
+mkdir -p ~/.config/plank/dock1
 cd plank
 cd dock1
 cp -r * ~/.config/plank/dock1
@@ -18,8 +19,8 @@ cd xfce4
 cp -r * ~/.config/xfce4
 cd ..
 
-echo "Insert deadbeef, plank, volumeicon into startup session"
+echo "Insert audacious, plank, volumeicon into startup session"
 
-echo "Greybird-dark-mx; Papirus-dark; In mouse do Bibata-Modern_Ice"
+echo "Greybird-dark-mx; Papirus-dark or Kora; In mouse do Bibata-Modern_Ice"
 
 echo "For yt-dlp use BACKPORTS"
